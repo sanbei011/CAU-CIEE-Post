1cf8c3c8953cb58428e8acfb602699d049d1fa45
Sanbei <sanbei101@outlook.com>
2024-03-17 22:14:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ announcements = {}
 for i, (day, year_month) in enumerate(time_matches):
     full_date = f"{year_month}-{day}"
     announcements[full_date] = title_matches[i]
-today = datetime(2024, 3, 8)
+
+today = datetime.now().date()
+# today = datetime(2024, 3, 8)
 filtered_announcements = {}
 for full_date, title in announcements.items():
     date = datetime.strptime(full_date, "%Y-%m-%d")
